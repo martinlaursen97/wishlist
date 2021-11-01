2cba04a2e49b42c085d281a837168efa13696045
Jakob <jakobmajid00@gmail.com>
2021-11-01 14:28:17 +0100

    added HTML and CSS

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
+    @GetMapping("/index2")
+    public String index2() {
+        return "index2";
     }
 }
