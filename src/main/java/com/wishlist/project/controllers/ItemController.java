add660e144edec69c50e7661a03f54309e3180c6
Jakob <jakobmajid00@gmail.com>
2021-11-04 19:41:32 +0100

    additem html

--- src/main/java/com/wishlist/project/controllers/ItemController.java
+++ src/main/java/com/wishlist/project/controllers/ItemController.java
@@ -5,6 +5,8 @@ import com.wishlist.project.domain.services.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class ItemController {
@@ -12,12 +14,12 @@ public class ItemController {
     private final ItemService itemService;
 
     @Autowired
-    public ItemController (ItemService itemService){
+    public ItemController(ItemService itemService) {
         this.itemService = itemService;
     }
 
     @GetMapping("/createItem")
-    public String createItem(){
+    public String createItem() {
         String name = "test";
         long wishListId = 12345678;
         String imageUrl = "url";
@@ -27,5 +29,21 @@ public class ItemController {
         itemService.createItem(name, wishListId, imageUrl, price, location, notes);
 
         return "index";
+    }
+
+    @GetMapping("/addItem")
+    public String addItem() {
+        return "addItem";
+    }
+
+    @PostMapping("/addItemVerify")
+    public String addItemVerify(WebRequest request) {
+        String name = request.getParameter("name");
+        String imageUrl = request.getParameter("imageUrl");
+        double price = Double.parseDouble(request.getParameter("price"));
+        String location = request.getParameter("location");
+        String notes = request.getParameter("notes");
+        itemService.createItem(name,1l,imageUrl,price,location,notes);
+        return "wishlist";
     }
 }
